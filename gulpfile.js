ece78e95a3607b4b4741730c2c15986fb0f9cf7d
Jason Brown <loktar69@hotmail.com>
2015-02-05 13:35:14 -0600

    Added release flag.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp                = require('gulp'),
     $                   = require('gulp-load-plugins')(),
     browserSync         = require('browser-sync'),
-    webpack             = require('gulp-webpack');
-
-var DEST = './build',
+    webpack             = require('gulp-webpack'),
+    argv                = require('minimist')(process.argv.slice(2)),
+    DEST = './build',
+    RELEASE = !!argv.release,
     src = {};
 
 gulp.task('default', ['serve']);
 
+// Run on server, or to test minification ect. doesn't spin up local server
+gulp.task('build', ['pages', 'bundle']);
+
+// Starts local server auto reloads, ect.
 gulp.task('browser-sync', function(){
     browserSync({
         notify: true,
@@ -17,26 +22,32 @@ gulp.task('browser-sync', function(){
     });
 });
 
+// markup task
 gulp.task('pages', function(){
     src.pages = './src/**/*.html';
     return gulp.src(src.pages)
         .pipe(gulp.dest(DEST));
 });
 
+// Pass everything through webpack. 
+// TODO remove dependency on gulp and just use webpack
 gulp.task('bundle', function(cb){
-    var config = require('./webpack.config.js')();
+    var config = require('./webpack.config.js')(RELEASE);
 
     return gulp.src('src/entry.js')
         .pipe(webpack(config, null, function(err, stats) {
             if(err) {
                 throw new $.util.PluginError('webpack', err);
             }
-            $.util.log('[webpack]', stats.toString({colors: true}));
+            if(!RELEASE){
+                $.util.log('[webpack]', stats.toString({colors: true}));
+            }
         }))
         .pipe(gulp.dest('build/'))
-        .pipe(browserSync.reload({stream:true}));;
+        .pipe($.if(RELEASE, browserSync.reload({stream:true})))
 });
 
+// Watches for changes.
 gulp.task('serve', ['browser-sync', 'pages', 'bundle'], function(cb){
     gulp.watch(src.pages, ['pages', browserSync.reload]);
     cb();
--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,32 @@ module.exports = function(release){
             path: __dirname + '/build',
             filename: 'app.js',
         },
-        watch : true,
+        plugins: release ? [
+          new webpack.optimize.UglifyJsPlugin(),
+          new webpack.optimize.OccurenceOrderPlugin(true),
+          new webpack.optimize.AggressiveMergingPlugin()
+        ] : [],
+        watch : !release,
         module: {
             preLoaders: [
                 { test: /\.js$/, exclude: /node_modules/, loader: 'jscs-loader'},
                 { test: /\.jsx$/, exclude: /node_modules/, loader: 'jscs-loader'}
             ],
             loaders: [
-                { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
+                { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
                 { test: /\.css$/, loader: 'style-loader!css-loader' },
                 { test: /\.jsx$/, loader: 'jsx-loader!6to5-loader' },
                 { test: /\.js$/, exclude: /node_modules/, loader: '6to5-loader'}
             ]
         },
         jscs : {
-            'preset': 'airbnb', 
+            // https://github.com/airbnb/javascript
+            'preset': 'airbnb',
+            // for es6 
             'esnext': true, 
+            // for jsx
             'esprima' : 'esprima-fb',
+            // overriding airbnb default styles
             'validateIndentation' : 4,
             "validateLineBreaks": "CRLF",
         }
